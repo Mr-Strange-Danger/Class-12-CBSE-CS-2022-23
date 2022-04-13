05fad91e5b8b66063eff6f8d8c29e6715f8dd8c2
Mr-Strange-Danger <99674248+Mr-Strange-Danger@users.noreply.github.com>
2022-04-13 18:05:44 +0530

    Write the Program that print the Table in 2 columns on the left integer temperature between 0 to 100 F and in right columns it should be in C
    Write the Program that print the Table in 2 columns on the left integer temperature between 0 to 100 F and in right columns it should be in C

---
+++ #Homework/Write the Program that print the Table in 2 columns on the left integer temperature between 0 to 100 F and in right columns it should be in C/Final_Project.py
@@ -0,0 +1,221 @@
+#Using Visual Studio Code
+#Main User using Git Service with GitHub
+#Made on apr 12 14:50:52 2022
+
+
+#Write the Program that print the Table in 2 columns on the left integer temperature between 0 to 100 F and in right columns it should be in C.
+
+print(' ___________________________________________________________')
+for CT in range(0,10):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,'  °C    | ',FT,'  °F                 | ',KT,' °K   | ')
+CT1=10
+FT1=(CT1*1.8)+32
+KT1=float(CT1)+273.15
+LC1=len(str(CT1))
+print(' | ',CT1,' °C    | ',FT1,'  °F                 | ',KT1,' °K   | ')
+for CT in range(11,13):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,'  °F                 | ',KT,' °K   | ')
+CT2=13
+FT2=(CT2*1.8)+32
+KT2=float(CT2)+273.15
+LC2=len(str(CT2))
+print(' | ',CT2,' °C    | ',FT2,'  °F   | ',KT2,' °K   | ')
+for CT in range(14,21):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,'  °F                 | ',KT,' °K   | ')
+CT3=21
+FT3=(CT3*1.8)+32
+KT3=float(CT3)+273.15
+LC3=len(str(CT3))
+LF3=len(str(FT3))
+LK3=len(str(KT3))
+print(' | ',CT3,' °C    | ',FT3,'  °F    | ',KT3,' °K   | ')
+for CT in range(22,26):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,'  °F                 | ',KT,' °K   | ')
+CT=26
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,'  °F    | ',KT,' °K   | ')
+for CT in range(27,31):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,'  °F                 | ',KT,' °K   | ')
+CT=31
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,'  °F    | ',KT,' °K   | ')
+for CT in range(32,37):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,'  °F                 | ',KT,' °K   | ')
+CT=37
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,'  °F    | ',KT,' °K   | ')
+for CT in range(38,42):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=42
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(43,47):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=47
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(48,52):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=52
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(53,57):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=57
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(58,62):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=62
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(63,67):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=67
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(68,74):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=74
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(75,79):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=79
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(80,84):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=84
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(85,89):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=89
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(90,94):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=94
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+for CT in range(95,99):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    LC=len(str(CT))
+    LF=len(str(FT))
+    LK=len(str(KT))
+    print(' | ',CT,' °C    | ',FT,' °F                 | ',KT,' °K   | ')
+CT=99
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,' °C    | ',FT,' °F    | ',KT,' °K   | ')
+CT=100
+FT=(CT*1.8)+32
+KT=float(CT)+273.15
+print(' | ',CT,'°C    | ',FT,' °F                 | ',KT,' °K   | ')
+print(' -----------------------------------------------------------')
+
+
+#Project Completed at apr 12 15:58:44 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
---
+++ #Homework/Write the Program that print the Table in 2 columns on the left integer temperature between 0 to 100 F and in right columns it should be in C/Try_1.py
@@ -0,0 +1,10 @@
+#Using Visual Studio Code
+#Main User using Git Service with GitHub
+#Made on apr 12 14:04:52 2022
+
+for CT in range(0,101):
+    FT=(CT*1.8)+32
+    KT=float(CT)+273.15
+    print(" -> ",CT," °C    ",FT," °F    ",FT," K")
+    
+    
