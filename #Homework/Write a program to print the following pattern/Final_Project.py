2de0840f324ddc3421204ac881e8b21c8e8e5152
Mr-Strange-Danger <99674248+Mr-Strange-Danger@users.noreply.github.com>
2022-04-13 19:08:37 +0530

    Write the program to print the following Pattern
    Write the program to print the following Pattern

---
+++ #Homework/Write a program to print the following pattern/Final_Project.py
@@ -0,0 +1,22 @@
+#Project Completed at apr 12 17:58:44 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
+
+#Write the program to printthe following Pattern
+
+a='*'
+b=' '
+print(a,a,a,a,a,a,a,a)
+print(b,b,b,b,b,b,a)
+print(b,b,b,b,b,a)
+print(b,b,b,b,a)
+print(b,b,b,a)
+print(b,b,a)
+print(b,a)
+print(a,a,a,a,a,a,a,a)
+
+
+
+#Project Completed at apr 12 18:02:56 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
