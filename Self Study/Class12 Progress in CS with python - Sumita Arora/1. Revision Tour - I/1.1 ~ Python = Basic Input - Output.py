5e3fac544f9f981e19c2118c862da212d4556691
Mr-Strange-Danger <99674248+Mr-Strange-Danger@users.noreply.github.com>
2022-04-15 01:21:58 +0530

    new

---
+++ Self Study/Class12 Progress in CS with python - Sumita Arora/1. Revision Tour - I/1.1 ~ Python = Basic Input - Output.py
@@ -0,0 +1,77 @@
+#Project Completed at apr 14 11:58:44 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
+
+
+#Page no. 2 to 6
+#%%
+#Q1.(a , b and c)
+name1=str(input(' Enter Your Name : \n >>>  '))
+age1=int(input(' Enter Your Age : \n >>>  '))
+#name2=name1.capitalize
+print(' You are ',name1)
+print(' You are',age1,'years old')
+#%%
+#Q2 , Q3 and Q4 are question of Thery and Your knowlege about Python 
+#%%
+#Q6
+print('These things I have sPoken unto you,')
+print('         that in me yEs might have peace.')
+print('   In the world ye shAll have tribulation:')
+print('      but be of good Cheer;')
+print('       I have overcomE the world.')
+#%%
+#Q7 (a)
+print("    ______________                                ________________")
+print("   /              \                              / /            \ \ ")
+print("  / /        \ \   \                            |    -    -        \ ")
+print("  |                |                            | /          -  \  | ")
+print("  /                /                            \                  \ ")
+print("  |  __ \  \|| /  /                              \_____________  \  \ ")
+print("  |  /           |                               |             \    | ")
+print("  |  |     __    |                               |              \   \ ")
+print(" /   |       \   |                               |               \  | ")
+print(" |   |        \  |                               |  ====          | | ")
+print(" |   |       __  |                               |  (o-)      _   | | ")
+print(" |  __\     (_o) |                              /             \  |  | ")
+print(" | |             |        Heh Heh Heh          /             ) ) |  | ")
+print(" \ ||             \       /      Huh Huh Huh  /              ) / |  | ")
+print(" | |__             \     /                \   |___             - |  | ")
+print(" | |           (*___\   /                  \     *'              |  | ")
+print(" | |     _      |      /                    \   |____            |  | ")
+print(" | |   //_______|                                ####\           |  | ")
+print(" | /     |_|_|_|___/\                            ------          |_/  ")
+print(" \|     \ -         |                            |                |   ")
+print("  |     _----_______/                            \_____           |   ")
+print("  |    /                                              \           |   ")
+print("  |   /                                                \          |   ")
+#%%
+#Q7 (b)
+print("            .- - - - -.  ")
+print("           ( #-... ' ` \  ")
+print("            \ #        | ")
+print("            _)\" ==== \"|_ ")
+print("           (_`\"======\"`_) ")
+print("            /`\"\"\"\"\"\"\"\"` \ ")
+print("           |        o  _o\ ")
+print("           |        ( _> |  ")
+print("           \       ' .___/--# ")
+print("            '.     ;-._:'\ ")
+print("              )===\   <>_/     __ ")
+print("       .---\"\" `====` - ` \__. ' `| ")
+print("      /                ()\      / ")
+print("      \___..--'           \_.-' ")
+print("         |              ()| ")
+print("         ;                ; ")
+print("         \             ()/ ")
+print("          \         '.  / ")
+print("       _.' `\         '; ")
+print("      (      `\        \_ ")
+print("       \    .- `\        `\ ")
+print("        \____)   `._____ .' ")
+
+
+
+#Project Completed at apr 14 13:28:56 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
---
+++ Self Study/Class12 Progress in CS with python - Sumita Arora/1. Revision Tour - I/1.2 ~ Data Handling.py
@@ -0,0 +1,13 @@
+#Project Completed at apr 14 11:58:44 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
+
+
+
+
+
+
+
+#Project Completed at apr 14 13:28:56 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
