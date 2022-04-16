776c21245f8eb28231ac4780fd1927fa83ead5bb
Mr-Strange-Danger <99674248+Mr-Strange-Danger@users.noreply.github.com>
2022-04-16 15:59:57 +0530

    new

--- Self Study/Class12 Progress in CS with python - Sumita Arora/1. Revision Tour - I/1.2 ~ Data Handling.py
+++ Self Study/Class12 Progress in CS with python - Sumita Arora/1. Revision Tour - I/1.2 ~ Data Handling.py
@@ -3,7 +3,31 @@
 #Support Visual Studio Code
 
 
+#%%
+#Q11 :
+pe1=int(input(' Enter Perimeter of The Square :  '))
+print('Side of The Square :  ',pe1/4)
 
+#%%
+#Q12 :
+dif=1056
+diy=dif/3
+dim=dif/1760
+print(' Distance in feet = ',dif,'feet\n Distance in yard = ',diy,'yard\n Distance in miles = ',dim,'miles')
+
+#%%
+#Q13 :
+sd1,sd2,sd3=30,25,35
+prm=sd1+sd2+sd3
+rod=560/prm
+rof=int(rod)
+print('\n The Athlete has Completed',rof,'round of a Triangular Park')
+
+#%%
+#Q14 :
+yr,sd,hr,dy=365,60,60,24
+sey=yr*sd*hr*dy
+print(' No. of Seconds in A Year  = ',sey,'years')
 
 
 
