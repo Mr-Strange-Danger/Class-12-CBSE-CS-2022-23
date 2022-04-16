bdd39930d0d5ba2d446a2ca78b0caa718976b31e
Mr-Strange-Danger <99674248+Mr-Strange-Danger@users.noreply.github.com>
2022-04-17 00:28:41 +0530

    0 3

---
+++ Self Study/Class12 Progress in CS with python - Sumita Arora/2. Revision Tour - II/2.2 ~ Python Sequences = Dictionaries, Sorting.py
@@ -0,0 +1,26 @@
+#Using Visual Studio Code
+#Main User using Git Service with GitHub
+#Made on apr 16 23:04:52 2022
+
+
+#%%
+#Q7 :
+import string
+
+
+telebook={'Jivin':'85000-00003'}
+for ui in range(2):
+    nm=(input('Enter The Name  : '))
+    ph=(input('Enter The Phone Number  : '))
+    temp_tb=dict()
+    temp_tb['name']=nm
+    temp_tb['phone']=ph
+    telebook.append(temp_tb)
+
+
+
+
+
+#Project Completed at apr 16 23:36:46 2022
+#Project Completed with Visual Studio Code
+#Support Visual Studio Code
