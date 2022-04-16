3a694879a5533c2f8b5667408765bf00d0e9eb61
Mr-Strange-Danger <99674248+Mr-Strange-Danger@users.noreply.github.com>
2022-04-16 23:45:39 +0530

    0

--- Self Study/Class12 Progress in CS with python - Sumita Arora/2. Revision Tour - II/2.1 ~ Python Sequences = Strings, Lists and Tuples.py
+++ Self Study/Class12 Progress in CS with python - Sumita Arora/2. Revision Tour - II/2.1 ~ Python Sequences = Strings, Lists and Tuples.py
@@ -1,8 +1,79 @@
 #Using Visual Studio Code
 #Main User using Git Service with GitHub
-#Made on apr 12 14:04:52 2022
+#Made on apr 16 16:04:52 2022
 
-L
+
+#%%
+#Q2 :
+str1="nectarine"
+o1,o2,o3,o4=str1[0],str1[2:4],str1[-4],str1[-2]
+print(o1+o2+o3+o4)
+str2="blueberry"
+p1,p2,p3=str2[0:2],str2[4],str2[6:9]
+print(p1+p2+p3)
+
+#%%
+#Q3 :
+istr,vl5,ostr1 = input("Enter String "),['a','e','i','o','u'],''
+for s in istr:
+    if s in vl5:
+        ostr1 +='({})'.format(s)
+    else:
+        ostr1 +=s
+print(ostr1)
+
+#%%
+#Q7 :
+istr1 = input("Enter list elements space separated : ")
+ilt = list(map(int ,istr1.split()))
+del ilt[0]
+print(ilt)
+#%%
+#Q9 :
+tpi1 = (1, 2, 3)
+tpi2 = (4, 5, 6)
+tpi3 = tpi1 +  tpi2
+print(tpi3)
+#%%
+#Q10 :
+yu1 = [3, 4, 1, 5, 8, 2]
+yu1.sort()
+print("Output using inbuilt function : {}".format(yu1))
+#%%
+#Q11 :
+#method_1
+tyj = (3, 4, 1, 5, 8, 2)
+loku = list(tyj)
+ui = len(loku)
+for h1 in range(ui):
+    for h2 in range(ui):   
+        if(loku[h1]<loku[h2]):
+            temp = loku[h1]
+            loku[h1] = loku[h2]
+            loku[h2] = temp
+tyj = tuple(loku)
+print("Output of tuple to list approach : {}".format(tyj))
+#%%
+#Q12 :
+a = [[1,2,3],
+     [4,5,6],
+     [7,8,9]]
+row = len(a)
+col = len(a[0])
+n = row*col//2
+count = 0
+for r in range(row):
+    for c in range(col):
+        if count<=n:
+            temp = a[r][c]
+            a[r][c] = a[row-1-r][col-1-c]
+            a[row-1-r][col-1-c] = temp
+            count+=1 
+for i in  range(row) :  
+    for j in range(col) : 
+        print(a[i][j],end= ' '); 
+    print(); 
+
 
 
 
